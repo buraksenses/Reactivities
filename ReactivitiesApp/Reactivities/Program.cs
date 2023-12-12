22497d5d796b60f1f203b1cc310f24a1ec15048a
Burak Şenses <buraksenseees@gmail.com>
2023-12-12 18:36:07 +0300

    data seed

--- ReactivitiesApp/Reactivities/Program.cs
+++ ReactivitiesApp/Reactivities/Program.cs
@@ -31,7 +31,8 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
 }
 catch (Exception e)
 {
