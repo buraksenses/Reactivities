16736ae91c8ef73d53dbff81987469368e32f163
Burak Şenses <buraksenseees@gmail.com>
2023-12-12 19:02:19 +0300

    activities controller

---
+++ ReactivitiesApp/Reactivities/Controller/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Data.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.Controller;
+
+[ApiController]
+[Route("api/activities")]
+public class ActivitiesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetAll()
+    {
+        return await _context.Activities.ToListAsync();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<ActionResult<Activity>> GetById([FromRoute] Guid id)
+    {
+        return await _context.Activities.FindAsync(id);
+    }
+}
