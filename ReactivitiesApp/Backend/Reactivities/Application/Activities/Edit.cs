c6bdda64d5f4b37eb081e771c6edd5479ad03a58
Burak Şenses <buraksenseees@gmail.com>
2023-12-13 18:06:45 +0300

    create and edit activity

---
+++ ReactivitiesApp/Backend/Reactivities/Application/Activities/Edit.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Reactivities.Data.Domain;
+
+namespace Reactivities.Application.Activities;
+
+public class Edit
+{
+    public class Command : IRequest
+    {
+        public Activity Activity { get; set; }
+    }
+}
--- ReactivitiesApp/Backend/Reactivities/Controller/ActivitiesController.cs
+++ ReactivitiesApp/Backend/Reactivities/Controller/ActivitiesController.cs
@@ -1,10 +1,7 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Reactivities.Application.Activities;
 using Reactivities.Controller.Base;
 using Reactivities.Data.Domain;
-using Reactivities.Persistence;
 
 namespace Reactivities.Controller;
 
@@ -22,6 +19,14 @@ public class ActivitiesController : BaseApiController
     [Route("{id}")]
     public async Task<ActionResult<Activity>> GetById([FromRoute] Guid id)
     {
+        return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] Activity activity)
+    {
+        await Mediator.Send(new Create.Command { Activity = activity });
+        
         return Ok();
     }
 }
