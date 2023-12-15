77bc0693f6d598c7fefc663cc828ecbb2d0562cc
Burak Şenses <buraksenseees@gmail.com>
2023-12-15 18:43:46 +0300

    activity create and edit form

--- ReactivitiesApp/Frontend/client-app/src/app/layout/NavBar.tsx
+++ ReactivitiesApp/Frontend/client-app/src/app/layout/NavBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
 
-export default function NavBar() {
+interface Props {
+  openForm: (id?: string) => void;
+}
+
+export default function NavBar({ openForm }: Props) {
   return (
     <Menu inverted fixed="top">
       <Container>
@@ -16,7 +20,11 @@ export default function NavBar() {
         <Menu.Item name="Activities" />
 
         <Menu.Item>
-          <Button positive content="Create Activity" />
+          <Button
+            positive
+            content="Create Activity"
+            onClick={() => openForm()}
+          />
         </Menu.Item>
       </Container>
     </Menu>
--- ReactivitiesApp/Frontend/client-app/src/features/activities/dashboard/ActivityList.tsx
+++ ReactivitiesApp/Frontend/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -3,9 +3,10 @@ import { Activity } from "../../../app/models/activity";
 
 interface Props {
   activities: Activity[];
+  selectActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities }: Props) {
+export default function ActivityList({ activities, selectActivity }: Props) {
   return (
     <Segment>
       <Item.Group>
@@ -21,7 +22,12 @@ export default function ActivityList({ activities }: Props) {
                 </div>
               </Item.Description>
               <Item.Extra>
-                <Button floated="right" content="View" color="blue" />
+                <Button
+                  floated="right"
+                  content="View"
+                  color="blue"
+                  onClick={() => selectActivity(activity.id)}
+                />
                 <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
